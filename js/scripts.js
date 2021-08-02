11688a27e6a1b7b006df8fe0067f0b81d3813952
AdamVigeantUX <87435731+AdamVigeantUX@users.noreply.github.com>
2021-08-02 16:51:17 -0400

    Fix navigation icon colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 const navClick = () => {
   var x = document.getElementById("links");
+  var y = document.getElementById("hamIcon");
   if (x.style.display === "flex") {
     x.style.display = "none";
+    y.setAttribute("class", "fa fa-bars fa-lg navClosed");
   } else {
     x.style.display = "flex";
+    y.setAttribute("class", "fa fa-close fa-lg navOpened");
   }
 };
 
