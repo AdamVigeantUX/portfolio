7d138a614210687d4444382c5d318f0d9721eb46
AdamVigeantUX <87435731+AdamVigeantUX@users.noreply.github.com>
2021-08-25 10:20:38 -0400

    Update Colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,69 @@
 const navClick = () => {
-  var x = document.getElementById("links");
-  var y = document.getElementById("hamIcon");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-    y.setAttribute("class", "fa fa-bars fa-lg navClosed");
+  const x = document.getElementById('links')
+  const y = document.getElementById('hamIcon')
+  if (x.style.display === 'flex') {
+    x.style.display = 'none'
+    y.setAttribute('class', 'fa fa-bars fa-lg navClosed')
   } else {
-    x.style.display = "flex";
-    y.setAttribute("class", "fa fa-close fa-lg navOpened");
+    x.style.display = 'flex'
+    y.setAttribute('class', 'fa fa-close fa-lg navOpened')
   }
-};
+}
 
-console.log(`Let\'s go! This website\'s power level is over ${8999 + 1}!`);
+console.log(`Let's go! This website's power level is over ${8999 + 1}!`)
 
 /* Home Hero Text */
-var tl = gsap.timeline();
-tl.to("#heroText", {text:"Design Thinker", duration: 1, delay:2});
-tl.to("#heroText", {text:"Creative Problem Solver", duration: 1, delay:3});
-tl.to("#heroText", {text:"U", duration: .1, delay:3});
-tl.to("#heroText", {text:"UX Designer", duration: 3, ease:"power3.out"});
+const tl = gsap.timeline()
+tl.to('#heroText', { text: 'Design Thinker', duration: 1, delay: 2 })
+tl.to('#heroText', { text: 'Creative Problem Solver', duration: 1, delay: 3 })
+tl.to('#heroText', { text: 'U', duration: 0.1, delay: 3 })
+tl.to('#heroText', { text: 'UX Designer', duration: 3, ease: 'power3.out' })
 
 /* Home Project Reveal */
-gsap.from("#soulUnits", {y:25, opacity:0, duration:1.5, delay: .5, ease:"power3.out"});
-gsap.from("#sapInc", {y:25, opacity:0, duration:1.5, delay: 1, ease:"power3.out"});
+gsap.from('#soulUnits', {
+  y: 25,
+  opacity: 0,
+  duration: 1.5,
+  delay: 0.5,
+  ease: 'power3.out'
+})
+gsap.from('#sapInc', {
+  y: 25,
+  opacity: 0,
+  duration: 1.5,
+  delay: 1,
+  ease: 'power3.out'
+})
 
 /* Project Hero Image Reveal */
-gsap.from(".heroImg", {x:100, opacity:0, duration:1.5, delay:.25, ease:"power3.out"});
+gsap.from('.heroImg', {
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+  delay: 0.25,
+  ease: 'power3.out'
+})
 
 /* Project Hero Image Reveal */
-gsap.from("#headshot", {x:-100, opacity:0, duration:1.5, delay:.25, ease:"power3.out"});
+gsap.from('#headshot', {
+  x: -100,
+  opacity: 0,
+  duration: 1.5,
+  delay: 0.25,
+  ease: 'power3.out'
+})
 
 /* Button Hover Effect */
-$(".button").mouseenter(function(){gsap.to(this, {y:-5});});
-$(".button").mouseleave(function(){gsap.to(this, {y:0});});
+$('.button').mouseenter(function () {
+  gsap.to(this, { y: -5 })
+})
+$('.button').mouseleave(function () {
+  gsap.to(this, { y: 0 })
+})
 
-$("#logo").mouseenter(function(){gsap.to(this, {y:-3, opacity:.8});});
-$("#logo").mouseleave(function(){gsap.to(this, {y:0, opacity:1});});
+$('#logo').mouseenter(function () {
+  gsap.to(this, { y: -3, opacity: 0.8 })
+})
+$('#logo').mouseleave(function () {
+  gsap.to(this, { y: 0, opacity: 1 })
+})
